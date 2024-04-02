80f84a6fb03e324f1b1714124de188dc659e11e3
Triệu Quốc Đạt <127685811+TiensiDua@users.noreply.github.com>
2024-04-02 08:12:22 +0700

    Add files via upload

---
+++ Endline.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    // Xuống dòng bằng "\n"
+    std::cout << "Hello "
+              << "\n";
+
+    // Xuống dòng bằng '\n'
+    std::cout << "Hihi " << '\n';
+
+    std::cout << "Xin chao " << '\n';
+
+    std::cout << "Hello\nWorld"
+              << '\n';
+
+    std::cout << "Hello" << "\n"
+              << "World";
+
+    return 0;
+}
---
+++ HelloNoSTD.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    // Xuống dòng bằng '\n'
+    std::cout << "Hello World \nHihi \n";
+
+    // Xuống dong bằng '\n'
+    std::cout << "Welcome to C++"
+              << '\n';
+
+    // Xuống dòng bằng R"(...)" << '\n'
+    std::cout << R"(C++ is my favorite programing language
+    Some programing language
+    C
+    Java
+    C#
+    Python)"
+              << '\n';
+
+    return 0;
+
+    //
+}
